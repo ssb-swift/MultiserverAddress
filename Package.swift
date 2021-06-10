8c1f10d757415beb48351b6f64c866aa965ca6a8
David Gomez <dg.dave@hey.com>
2021-06-10 10:19:05 -0400

    Simplify Package.swift file

--- Package.swift
+++ Package.swift
@@ -13,9 +13,7 @@ let package = Package(
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "MultiserverAddress",
-      targets: ["MultiserverAddress"])
+    .library(name: "MultiserverAddress",targets: ["MultiserverAddress"])
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -25,13 +23,7 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .target(
-      name: "MultiserverAddress",
-      dependencies: [
-        .product(name: "Regex", package: "Regex")
-      ]),
-    .testTarget(
-      name: "MultiserverAddressTests",
-      dependencies: ["MultiserverAddress"]),
+    .target(name: "MultiserverAddress", dependencies: ["Regex"]),
+    .testTarget(name: "MultiserverAddressTests",dependencies: ["MultiserverAddress"]),
   ]
 )
